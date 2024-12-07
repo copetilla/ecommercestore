cb47c7b4efa261f51bbe3e27c46ea3c8885e1be3
copetilla <gabrielgz2001@gmail.com>
2024-12-06 19:57:49 -0600

    Update accordion-pay.tsx

--- app/(routes)/pay/components/accordion-pay.tsx
+++ app/(routes)/pay/components/accordion-pay.tsx
@@ -2,6 +2,7 @@ import { Checkbox } from '@/components/ui/checkbox'
 import React from 'react'
 
 const AccordionPay = () => {
+
     return (
         <div className=''>
             <div className=' flex items-center px-3 py-4 rounded-t-md border border-1 gap-x-4'>
@@ -9,8 +10,9 @@ const AccordionPay = () => {
                 <p className='text-sm'>SINPE MOVIL</p>
             </div>
             <div className='flex items-center px-3 py-4 rounded-b-md bg-gray-100 gap-x-4'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni natus distinctio ducimus numquam suscipit vitae quisquam neque voluptatibus amet eius similique eligendi, nesciunt, possimus cumque aperiam provident a nobis illo.
-            </div>
+                SINPE Móvil: 84551337 a nombre de DIEGO ESTEBAN SABORIO
+                - Por favor agregar su nombre como detalle
+                - Enviar al mismo número de WhatsApp el comprobante para que nuestro equipo pueda iniciar procesando su orden            </div>
         </div>
     )
 }
