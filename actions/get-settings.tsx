1d7846d313c147128da3e1128959b3ff4aa8940a
copetilla <gabrielgz2001@gmail.com>
2024-12-11 13:29:22 -0600

    a

--- actions/get-settings.tsx
+++ actions/get-settings.tsx
@@ -1,6 +1,6 @@
 import { Settings } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL_STORES}/15812c1b-dc74-4ae2-b9c0-4998e2ba2a64`
+const URL = `${process.env.NEXT_PUBLIC_API_URL_STORES}/8bf0a680-cfc8-47af-a93c-ec1b16e8954e`
 
 const getSettings = async (): Promise<Settings> => {
 
