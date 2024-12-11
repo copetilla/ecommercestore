758e6d336eb25887f2c35816d3476ae9c8c469d1
copetilla <gabrielgz2001@gmail.com>
2024-12-10 23:36:42 -0600

    a

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -2,54 +2,41 @@ import getCategory from '@/actions/get-category';
 import getProducts from '@/actions/get-products';
 import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
-import React from 'react'
-import Filter from './components/filter';
-import NoResults from '@/components/ui/no-results';
 import ProductCard from '@/components/ui/product-card';
-import MobileFilters from './components/mobile-filter';
+import NoResults from '@/components/ui/no-results';
 
 export const revalidate = 0;
 
-interface CategoryPageProps {
+interface Props {
     params: {
         categoryId: string;
-    }
+    };
 }
 
-const CategoryPage = async ({ params }: CategoryPageProps) => {
-    const products = await getProducts({ categoryId: params.categoryId, isArchived: false })
-    const category = await getCategory(params.categoryId)
+export default async function CategoryPage({ params }: Props) {
+    const products = await getProducts({ categoryId: params.categoryId, isArchived: false });
+    const category = await getCategory(params.categoryId);
+
     return (
-        <div className='bg-white'>
+        <div className="bg-white">
             <Container>
                 <Billboard data={category.billboards} />
-                <div className='px-4 sm:px-6 lg:px-8 pb-24'>
-                    <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
-                        {/* mobile */}
-                        {/* <MobileFilters data={[{ id: '1', name: 'Tamaño' }, { id: '2', name: 'Grande' }]} valueKey='llaveId' name='Size' />
-                        <div className='hidden lg:block'>
-                            <Filter
-                                valueKey='llaveId'
-                                name='llaves'
-                                data={[{ id: '1', name: 'Tamano' }, { id: '2', name: 'Grande' }]}
-                            />
-                        </div> */}
-                        <div className='mt-6 lg:col-span-4 lg:mt-0'>
-                            {products.length === 0 && <NoResults />}
-                            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                                {products.map((item) => (
-                                    <ProductCard
-                                        key={item.id}
-                                        data={item}
-                                    />
-                                ))}
-                            </div>
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                    <div className="grid lg:grid-cols-5 lg:gap-x-8">
+                        <div className="lg:col-span-4">
+                            {products.length === 0 ? (
+                                <NoResults />
+                            ) : (
+                                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                    {products.map((item) => (
+                                        <ProductCard key={item.id} data={item} />
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </Container>
         </div>
-    )
+    );
 }
-
-export default CategoryPage
